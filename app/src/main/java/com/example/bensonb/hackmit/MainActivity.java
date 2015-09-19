790df1b3a59e6563f4e78251af18b6faee13ba04
mbhatg <mbhatg@gmail.com>
2015-09-19 19:40:16 -0400

    Parse in!

--- app/src/main/java/com/example/bensonb/hackmit/MainActivity.java
+++ app/src/main/java/com/example/bensonb/hackmit/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.bensonb.hackmit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "7uSbWIp5mdcf1dgUnYiEkgyL1rL6BeBz5bYvkoRA", "AAB4OWTqlgWy38bN1mZXWIFyRxbkdphbwXFc3uGM");
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -30,8 +43,14 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+
         }
 
         return super.onOptionsItemSelected(item);
+
+    }
+    public void toSendInfo(View view){
+        Intent intent = new Intent(this, SendInfoActivity.class);
+        startActivity(intent);
     }
 }
