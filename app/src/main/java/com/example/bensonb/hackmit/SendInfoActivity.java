f9ebe87165126929fffd4a236c7b85e368846b41
Brandon Benson <bensonb@mit.edu>
2015-09-19 17:47:07 -0500

    added the first three pages to the app

---
+++ app/src/main/java/com/example/bensonb/hackmit/SendInfoActivity.java
@@ -0,0 +1,9 @@
+package com.example.bensonb.hackmit;
+
+import android.app.Activity;
+
+/**
+ * Created by bensonb on 9/19/2015.
+ */
+public class SendInfoActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/bensonb/hackmit/WaitActivity.java
@@ -0,0 +1,7 @@
+package com.example.bensonb.hackmit;
+
+/**
+ * Created by bensonb on 9/19/2015.
+ */
+public class WaitActivity {
+}
