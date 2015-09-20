a2055e28636867d9596495aa7048d4d94bced0c1
Brandon Benson <bensonb@mit.edu>
2015-09-19 19:12:56 -0500

    added transitions between first three pages

--- app/src/main/java/com/example/bensonb/hackmit/WaitActivity.java
+++ app/src/main/java/com/example/bensonb/hackmit/WaitActivity.java
@@ -1,7 +1,20 @@
 package com.example.bensonb.hackmit;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 /**
  * Created by bensonb on 9/19/2015.
  */
-public class WaitActivity {
+public class WaitActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wait);
+    }
+
 }
+
+
