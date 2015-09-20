bbcbc963633a9e62952dce111ec99172f560383e
mbhatg <mbhatg@gmail.com>
2015-09-19 20:28:13 -0400

    Create request object

--- app/src/main/java/com/example/bensonb/hackmit/SendInfoActivity.java
+++ app/src/main/java/com/example/bensonb/hackmit/SendInfoActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.parse.ParseObject;
 
 /**
  * Created by bensonb on 9/19/2015.
@@ -15,6 +18,23 @@ public class SendInfoActivity extends Activity {
         setContentView(R.layout.activity_sendinfo);
     }
     public void toWait(View view) {
+        // get info from text boxes
+        EditText complaint = (EditText)findViewById(R.id.complaint);
+        String complaintString = complaint.getText().toString();
+        EditText location = (EditText)findViewById(R.id.location);
+        String locationString = location.getText().toString();
+        EditText details = (EditText)findViewById(R.id.details);
+        String detailsString = details.getText().toString();
+
+        // create ParseObject request
+        ParseObject request = new ParseObject("Request");
+        request.put("issue", complaintString);
+        request.put("location", locationString);
+        request.put("details", detailsString);
+        request.saveInBackground();
+
+        // send push notification
+
         Intent intent = new Intent(this, WaitActivity.class);
         startActivity(intent);
     }
