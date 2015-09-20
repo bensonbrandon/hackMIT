ca1b948beeb84fdedbc8d3b92a4a35cc25a5eb71
Brandon Benson <bensonb@mit.edu>
2015-09-20 02:00:17 -0500

    added google maps

---
+++ app/src/main/java/com/example/bensonb/hackmit/AcceptRejectActivity.java
@@ -0,0 +1,22 @@
+package com.example.bensonb.hackmit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by bensonb on 9/19/2015.
+ */
+public class AcceptRejectActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wait);
+    }
+    public void toMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
